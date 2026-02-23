f246149464ffa676dc37d54fe6017158c2e783ad
firefoxic <firefoxic.dev@gmail.com>
2026-02-23 13:26:43 +0300

    Update `meow` to 14.1.0 version

--- bin/cli.js
+++ bin/cli.js
@@ -1,106 +1,41 @@
 #!/usr/bin/env node
 
-// oxlint-disable max-lines-per-function
-import { error } from "node:console"
 import { relative } from "node:path"
 
 import meow from "meow"
 
+import { CLI_HELP } from "../lib/constants.js"
 import { optimizeAssets, optimizeImages } from "../lib/index.js"
 
 let cli = meow(
 	{
-		help: `
-  Usage
-    $ optimize <command> [options]
-
-  Commands
-
-    assets
-        Prepares all graphic assets for frontend projects:
-          - converts touch.svg and/or 32.svg (optionally 16.svg) to favicons,
-          - optimizes SVG icons and generates CSS,
-          - converts images and adds metadata to data.json.
-
-    images
-        Just optimizes all images in specified directory:
-          - optimizes vector images
-          - converts raster images to avif (by default).
-
-  Options
-    -v, --version  Print version and exit
-    -v, --help     Print this help and exit
-
-  To get help on specific command, run:
-    $ optimize assets -h
-    $ optimize images -h
-
-`,
+		help: CLI_HELP.main,
 		importMeta: import.meta,
+		commands: [
+			`assets`,
+			`images`,
+		],
 	},
 )
 
+if (cli.flags.help || cli.flags.h) cli.showHelp(0)
 if (cli.flags.version || cli.flags.v) cli.showVersion(0)
 
-let [command] = cli.input
-
-switch (command) {
+switch (cli.command) {
 	case `assets`:
 		await createAssetsCli()
 		break
 	case `images`:
 		await createImagesCli()
 		break
-	// oxlint-disable-next-line no-undefined
-	case undefined:
-		if (cli.flags.help || cli.flags.h) cli.showHelp(0)
-		error(`Command not specified.`)
-		cli.showHelp(2)
-		break
 	default:
-		error(`Unknown command: ${command}`)
-		cli.showHelp(2)
+		cli.showHelp()
 		break
 }
 
 async function createAssetsCli () {
 	let assetsCli = meow(
-		`
-  Usage
-    $ optimize assets [options]
-
-  Options
-
-    -p, --public-directory      (default: "./public/")
-            Path to the directory with the static assets
-
-    -s, --shared-directory      (default: "./src/shared/")
-            Path to the directory with shared files
-
-    -d, --origin-density        (default: 2)
-            Pixel density in dppx of the raw raster images
-            (0 means 1, but without adding the density suffix to the filename)
-
-    -f, --target-formats         (default: ["avif", "webp"])
-            Output raster image formats
-            (to specify multiple formats, specify an option for each)
-
-    -r, --remove-origin         (default: true)
-            Remove the original raster files after successful processing
-
-    -m, --add-meta-data         (default: true)
-            Add metadata of the raster images to data.json file and
-            generate CSS file for the icons.
-
-    -h, --help
-            Print this help and exit
-
-  Examples
-
-    $ optimize assets
-    $ optimize assets --no-r
-
-`,
+		CLI_HELP.assets,
 		{
 			description: `Optimize all graphic assets for your frontend project:\n - convert touch.svg and/or 32.svg (optionally 16.svg) to favicons,\n - optimize SVG icons and generate CSS with custom properties for icons,\n - convert images and add metadata to data.json`,
 			importMeta: import.meta,
@@ -145,45 +80,12 @@ async function createAssetsCli () {
 	assetsCli.flags.sharedDirectory = relative(`.`, assetsCli.flags.sharedDirectory)
 	assetsCli.flags.publicDirectory = relative(`.`, assetsCli.flags.publicDirectory)
 
-	let options = { command, ...assetsCli.flags }
-
-	await optimizeAssets(options)
+	await optimizeAssets(assetsCli.flags)
 }
 
 async function createImagesCli () {
 	let imagesCli = meow(
-		`
-  Usage
-    $ optimize images [options]
-
-  Options
-
-    -i, --input-directory       (default: "./")
-            Path to the directory containing raw files.
-
-    -o, --output-directory      (default: value of -i)
-            Path to the directory where processed files will be placed.
-
-    -d, --origin-density        (default: 0)
-            Pixel density in dppx of the raw raster images
-            (0 means 1, but without adding the density suffix to the filename)
-
-    -f, --target-formats         (default: ["avif"])
-            Output raster image formats
-            (to specify multiple formats, specify an option for each)
-
-    -r, --remove-origin         (default: false)
-            Remove the original raster files after successful processing.
-
-    -h, --help
-            Print this help and exit
-
-  Examples
-
-    $ optimize images -i photos -r
-    $ optimize images -i photos -o optimized -f webp -f avif -r
-
-`,
+		CLI_HELP.images,
 		{
 			description: `Optimize vector and raster images in specific directory (current by default).`,
 			importMeta: import.meta,
